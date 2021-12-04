a179e49953d9a205e03d0ed49b1b2d355e752adb
neosarchizo <neosarchizo@gmail.com>
2021-12-04 12:25:57 +0900

    feat: update example

--- examples/PS_33D_I2C_test/PS_33D_I2C_test.ino
+++ examples/PS_33D_I2C_test/PS_33D_I2C_test.ino
@@ -14,38 +14,10 @@ void loop()
 
   if (ret == 0)
   {
-    Serial.print("Co2 : ");
+    Serial.print("pressure : ");
     Serial.println(ps_33d_i2c.pressure);
-    Serial.println("Status >>");
-
-    switch (cm1106_i2c.status)
-    {
-    case CM1106_I2C_STATUS_PREHEATING:
-    {
-      Serial.println("Preheating");
-      break;
-    }
-    case CM1106_I2C_STATUS_NORMAL_OPERATION:
-    {
-      Serial.println("Normal operation");
-      break;
-    }
-    case CM1106_I2C_STATUS_OPERATING_TROUBLE:
-    {
-      Serial.println("Operating trouble");
-      break;
-    }
-    case CM1106_I2C_STATUS_OUT_OF_FS:
-    {
-      Serial.println("Out of FS");
-      break;
-    }
-    case CM1106_I2C_STATUS_NON_CALIBRATED:
-    {
-      Serial.println("Non calibrated");
-      break;
-    }
-    }
+    Serial.print("status : ");
+    Serial.println(ps_33d_i2c.status);
   }
   delay(1000);
 }
